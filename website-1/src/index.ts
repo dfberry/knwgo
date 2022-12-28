f1b78b537c215b1bb42c2f5601570b44a79455c0
4ttedertlf <107864453+4ttedertlf@users.noreply.github.com>
2022-12-28 07:48:49 -0800

    Add basic api route for blobs, reset ts config

--- website-1/src/index.ts
+++ website-1/src/index.ts
@@ -1,7 +1,15 @@
 import express, { Express, Request, Response } from 'express';
 
+import { blobsRoute } from './routes/blobs';
+import { blobRoute } from './routes/blob';
+
 const app: Express = express();
 const port: number = Number(process.env.PORT) || 3000;
+
+
+
+app.use('/api/blobs', blobsRoute);
+app.use('/api/blob', blobRoute);
 
 app.get('/', (_: Request, res: Response) => {
   return res.send('Hello World');
